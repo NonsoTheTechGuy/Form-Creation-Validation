34345990f6af04b5bec41394840cb571206fa6d9
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-08-13 06:24:36 +0100

    Implemented Javascript to validate the Form.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
-    form.addEventListener('submit', function(event) {
+    document.addEventListener('submit', function(event) {
         event.preventDefault();
         
         const username = document.getElementById('username').value.trim();
